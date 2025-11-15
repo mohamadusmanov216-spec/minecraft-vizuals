523be17218e9c50e6d2d05d2e8571fec3c6f8217
mohamadusmanov216-spec <mohamadusmanov216@gmail.com>
2025-11-15 14:53:11 +0300

    Update AxmedVisuals.java

--- minecraft-mod/src/main/java/com/axmed555/visuals/AxmedVisuals.java
+++ minecraft-mod/src/main/java/com/axmed555/visuals/AxmedVisuals.java
@@ -7,6 +7,8 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,6 +27,8 @@ public class AxmedVisuals {
         ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
             () -> new ConfigScreenHandler.ConfigScreenFactory(
                 (mc, screen) -> new com.axmed555.visuals.gui.ConfigScreen(screen)));
+        
+        MinecraftForge.EVENT_BUS.register(this);
     }
     
     private void registerKeys(net.minecraftforge.client.event.RegisterKeyMappingsEvent event) {
@@ -44,7 +48,16 @@ public class AxmedVisuals {
     private void clientSetup(final FMLClientSetupEvent event) {
         MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
         MinecraftForge.EVENT_BUS.register(KeyInputHandler.class);
-        LOGGER.info("Axmed555 Visuals initialized!");
+        LOGGER.info("Visuals by axmed555 initialized!");
+    }
+
+    @SubscribeEvent
+    public void onScreenInit(ScreenEvent.Init event) {
+        // Кастомный начальный экран с "Visuals by axmed555"
+        if (event.getScreen() instanceof net.minecraft.client.gui.screens.TitleScreen) {
+            // Здесь будет код для красивого начального экрана
+            LOGGER.info("Custom title screen loaded: Visuals by axmed555");
+        }
     }
 
     public static void saveConfig() {
